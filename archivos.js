02dd32e6a47f0f6b0124b5974857cf11fbf08172
Nicolas <nikitolima123456789@gmail.com>
2022-10-11 11:40:06 -0300

    reentrega

--- archivos.js
+++ archivos.js
@@ -1,97 +1,109 @@
 const fs = require('fs');
 const path = require('path');
-const { json } = require('stream/consumers');
-const { deflateSync } = require('zlib');
 ruta = path.resolve(__dirname, './productos.txt');
 
 class Contenedor{
-    save(objeto){
-        fs.readFile(ruta, 'utf-8', (error, contenido) =>{
-            if(error){
-                console.log("Hubo un error")
-            }else{
-                let flag = true
-                const productos = JSON.parse(contenido)
-    
-                for (let el of productos){
-                    if (el.id === objeto.id){
-                        flag = false
-                    }
-                }       
-                if (flag){
-                    objeto.id = productos[productos.length-1].id + 1
-                    productos.push(objeto)
+    async save(objeto){
+        try {
+            await fs.promises.readFile(ruta, 'utf-8')
+            .then(contenido => {
+                if(contenido === "[]" || contenido === ""){                
+                    objeto.id = 1
+                    objeto = JSON.stringify(objeto)       
+                    fs.writeFileSync(ruta, `[${objeto}]`)
                     console.log(`ID del objeto: ${objeto.id}`)
                 }else{
-                    console.log("Ese id ya esta siendo utilizado")
-                }          
-                fs.writeFile(ruta, JSON.stringify(productos), error =>{
-                    if(error){
-                        console.log("Hubo un error")
-                    }
-                })        
-            }
-        })
-    }
-
-    getById(Id){
-        fs.readFile(ruta, 'utf-8', (error, contenido) =>{
-            const productos = JSON.parse(contenido)
-            if (error) {
-                console.log("Hubo un error")
-            }else{
-                let res = "No encontrado"
-                for (let el of productos){
-                    if (el.id === Id){
-                        res = el
-                    }
+                    let productos = JSON.parse(contenido)
+                    objeto.id = productos[productos.length-1].id + 1
+                    productos.push(objeto)
+                    productos = JSON.stringify(productos)
+                    console.log(`ID del objeto: ${objeto.id}`)
+                    fs.writeFileSync(ruta, `${productos}`)
                 }
-                console.log(res)
-            }
-        })
+            })
+            .catch(err =>{
+                console.log("error de lectura", err)
+            })
+        } catch (error) {           
+        }
     }
 
-    getAll(){
-        fs.readFile(ruta, 'utf-8', (error, contenido) => {
-            if (error){
-                console.log("Hubo un error")
-            }else{
-                console.log(JSON.parse(contenido))
-            }
-            
-        })
-    }
-
-    deleteByNumber(numero){        
-        fs.readFile(ruta, 'utf-8', (error, contenido) => {
-            if (error){
-                console.log("Hubo un error")
-            }else{
-                let productos = JSON.parse(contenido)
-                productos = productos.filter(el => el.id != numero)
-                fs.writeFile(ruta, JSON.stringify(productos), error =>{
-                    if(error){
-                        console.log("Hubo un error")
+    async getById(Id){
+        try{
+            await fs.promises.readFile(ruta, 'utf-8')
+            .then(contenido => {
+                if (contenido !== "" || contenido !== "[]"){
+                    const productos = JSON.parse(contenido)
+                    let res = "No encontrado"
+                    for (let el of productos){
+                        if (el.id === Id){
+                            res = el
+                        }
                     }
-                }) 
-            }
-        })
+                    console.log(res)
+                }           
+            })
+            .catch(err => {
+                console.log("error", err)
+            })
+        }
+        catch(error){
+        }
     }
 
-    deleteAll(){
-        fs.writeFile(ruta, `[]`, error =>{
-            if(error){
-                console.log("Hubo un error")
-            }
-        })
+    async getAll(){
+        try {
+            await fs.promises.readFile(ruta, 'utf-8')
+            .then(contenido => {
+                if(contenido !== "" || contenido !== "[]"){
+                    console.log(JSON.parse(contenido))
+                }else{
+                    console.log("El archivo esta vacio")
+                }
+            })
+            .catch(err => {
+                console.log("error", err)
+            })
+        } catch (error) {
+            
+        }
+    }
+
+    async deleteByNumber(numero){      
+        try {
+            await fs.promises.readFile(ruta, 'utf-8')
+            .then(contenido => {
+                if(contenido !== "" || contenido !== ""){
+                    let productos = JSON.parse(contenido)
+                    productos = productos.filter(el => el.id != numero)
+                    fs.writeFileSync(ruta, JSON.stringify(productos))
+                    console.log("Se elimino el producto")
+                }else{
+                    console.log("El archivo esta vacio")
+                }
+            })
+        }
+        catch(error){
+
+        }
+    }
+
+    async deleteAll(){
+        try{
+            await fs.promises.writeFile(ruta, `[]`)
+            console.log("Archivos borrados")
+        }
+        catch(err){
+
+        }
     }
 
 }
 
-const cont = new Contenedor
+const productos = new Contenedor
 
-//cont.save({"title": "Cartel blanco", "price": 2000, "thumbnail": "https://neondesign.netlify.app/img/Trabajo4.jpg"})
-cont.getAll();
-// cont.getById(1);
-//cont.deleteByNumber(4)
-// cont.deleteAll()
+//productos.save({"title": "Cartel blanco", "price": 2000, "thumbnail": "https://neondesign.netlify.app/img/Trabajo4.jpg"})
+//productos.getAll();
+//productos.getById(2);
+//productos.deleteByNumber(4)
+//productos.deleteAll()
