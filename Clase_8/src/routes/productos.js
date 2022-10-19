b0efeb301afc4c849c35b94f9409cad6ea4eabf3
Nicolas <nikitolima123456789@gmail.com>
2022-10-19 11:57:29 -0300

    entrega 8

---
+++ Clase_8/src/routes/productos.js
@@ -0,0 +1,125 @@
+const {Router} = require("express")
+const rutaProductos = Router()
+const fs = require("fs/promises")
+const path = require("path")
+
+const filePath = path.resolve(__dirname, "../../productos.txt")
+
+rutaProductos.get("/", async(req, res)=>{
+    const productos = await fs.readFile(filePath, "utf-8")
+    res.json({
+        data:JSON.parse(productos)
+    })
+})
+
+rutaProductos.get("/:id", async(req, res)=>{
+    const id = req.params.id
+    console.log(id)
+    const productos = JSON.parse(await fs.readFile(filePath, "utf-8"))
+    const indice = productos.findIndex(unproducto => unproducto.id == id)
+    
+    if(indice < 0){
+        return res.status(404).json(
+            {
+                msg: "El producto no existe"
+            }
+        )
+    }
+
+    res.json({
+        msg:`devolviendo el producto con id ${id}`,
+        data: productos[indice]
+    })
+})
+
+rutaProductos.post("/", async(req,res)=>{
+    const data = req.body;
+    console.log(req.body);
+    const fileData = await fs.readFile(filePath, "utf-8")
+    const productos = JSON.parse(fileData)
+
+    const {title, price, thumbnail} = req.body
+    const id = (productos[productos.length-1].id)+1
+    if(!title || !price || !thumbnail){
+        return res.status(400).json({
+            msg:"campos invalidos"
+        })
+    }
+
+    const nuevoProducto = {
+        title,
+        price,
+        thumbnail,
+        id
+    }
+
+    
+    
+    
+    productos.push(nuevoProducto)
+
+    await fs.writeFile(filePath, JSON.stringify(productos, null, "\t"))
+
+    res.json({
+        msg: "ok",
+        data: nuevoProducto
+
+    })
+
+})
+
+rutaProductos.delete("/:id", async(req,res)=>{
+    const id = req.params.id
+    const productos = JSON.parse(await fs.readFile(filePath, "utf-8"))
+    const indice = productos.findIndex(user => user.id == id)
+
+    if(indice < 0){
+        return res.json({
+            msg: "ok"
+        })
+    }
+
+    productos.splice(indice, 1)
+    await fs.writeFile(filePath, JSON.stringify(productos, null, "\t"))
+
+    res.json({
+        msg: `borrando al producto con id ${id}`,
+    })
+})
+
+rutaProductos.put("/:id", async(req,res)=>{
+    const id = req.params.id
+    const {title,price,thumbnail} = req.body
+    const productos = JSON.parse(await fs.readFile(filePath, "utf-8"))
+    const indice = productos.findIndex(user => user.id == id)
+    if(indice < 0){
+        return res.json({
+            msg: "ok"
+        })
+    }
+
+    if(!title || !price|| !thumbnail){
+        return res.status(400).json({
+            msg:"campos invalidos"
+        })
+    }
+
+    const prodActualizado = {
+        title,
+        price,
+        thumbnail,
+        id: productos[indice].id
+    }
+
+    productos.splice(indice,1, prodActualizado)
+
+    await fs.writeFile(filePath, JSON.stringify(productos, null, "\t"))
+    
+
+    res.json({
+        msg: `modificando producto con id: ${id}`,
+        data: prodActualizado
+    })
+})
+
+module.exports = rutaProductos
---
+++ Clase_8/src/services/server.js
@@ -0,0 +1,13 @@
+const express = require("express")
+const mainRouter = require("../routes/index.js")
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+
+
+
+app.use("/api", mainRouter)
+app.use(express.static("public"))
+module.exports = app
