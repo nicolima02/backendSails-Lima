c50bc89fd652d1b2bcb642d53f971a3ab291390d
Nicolas <nikitolima123456789@gmail.com>
2023-03-13 14:20:26 -0300

    entrega clase 46

---
+++ Prueba/Backend-Lima/README.md
@@ -0,0 +1,13 @@
+la ruta de productos es localhost:1337/products
+
+para postear se necesitan los siguientes parametros:
+{
+    "title": "",
+    "price": 1111,
+    "thumbnail": "",
+    "codigo": 11111,
+    "desc": "",
+    "stock": 1111
+}
+
+para borrar un producto se debe ir a la ruta localhost:1337/products/(id del producto)
---
+++ Prueba/Backend-Lima/api/models/Products.js
@@ -0,0 +1,24 @@
+/**
+ * Products.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    title: {type: 'string', required: true},
+    price: {type: 'number', required:true},
+    thumbnail: {type: 'string', required:true},
+    codigo: {type: 'number', required:true},
+    desc: {type: 'string', required: true},
+    stock: {type: 'number', required:true},
+    cant: {type:'number'}   
+
+  },
+
+  schema:true
+
+};
+
