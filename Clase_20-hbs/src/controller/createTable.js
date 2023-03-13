117aadd5566db27b3f60d94ab1be9f6d01b61f15
nicolima02 <102038516+nicolima02@users.noreply.github.com>
2023-03-13 14:40:47 -0300

    Delete Clase_20-hbs directory

--- Clase_20-hbs/src/controller/createTable.js
+++
@@ -1,8 +0,0 @@
-const ProductosSQL = require('./productos.js')
-const options = require('../../options/db')
-const ChatSQL = require('./chat.js')
-
-const ProductosController = new ProductosSQL(options)
-
-ProductosController.createTable()
-ChatSQL.createTable()
--- Clase_20-hbs/src/index.js
+++
@@ -1,7 +0,0 @@
-const server = require("./services/server.js")
-
-const puerto = process.env.PORT || 8080
-
-server.listen(puerto, ()=>{
-    console.log(`Servidor listo escuchando en el puerto ${puerto}` )
-})
